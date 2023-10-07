30fb96bad6dd13b72d7f48b56c8306a5ae89120b
nelsonAndelson <nelsonbaguma15@gmail.com>
2023-10-07 18:59:52 -0400

    took a scrnshot of page

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!!");
+const puppeteer = require("puppeteer");
+
+async function run() {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+
+  await page.goto("https://clevelandmetro.schoology.com/grades/grades");
+
+  await page.screenshot({ path: "example.png" });
+
+  await browser.close();
+}
+
+run();
